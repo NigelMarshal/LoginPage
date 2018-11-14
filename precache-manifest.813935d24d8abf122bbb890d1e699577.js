7107244396b90c7b36fbd8ded1287a104fc5c958
Nigel Marshal <nigelmarshal@gmail.com>
2018-11-14 16:06:38 +0400

    Updates

---
+++ precache-manifest.813935d24d8abf122bbb890d1e699577.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d08f03236335bda05fc570624b88cb58",
+    "url": "/NigelMarshal/LoginPage/static/media/product-card.d08f0323.png"
+  },
+  {
+    "revision": "895f7fce0d9622283cec51da06cd757a",
+    "url": "/NigelMarshal/LoginPage/static/media/carrefour-logo.895f7fce.png"
+  },
+  {
+    "revision": "9ff12f694e5951a6f51a9d63b05062e7",
+    "url": "/NigelMarshal/LoginPage/static/media/OpenSans-Light.9ff12f69.ttf"
+  },
+  {
+    "revision": "d8e91de1d2eb2b87fa87",
+    "url": "/NigelMarshal/LoginPage/static/js/runtime~main.d8e91de1.js"
+  },
+  {
+    "revision": "f6f23035b834e8d759c1",
+    "url": "/NigelMarshal/LoginPage/static/js/main.f6f23035.chunk.js"
+  },
+  {
+    "revision": "928d7a1b1be1870ccfa2",
+    "url": "/NigelMarshal/LoginPage/static/js/1.928d7a1b.chunk.js"
+  },
+  {
+    "revision": "f6f23035b834e8d759c1",
+    "url": "/NigelMarshal/LoginPage/static/css/main.3f768a5e.chunk.css"
+  },
+  {
+    "revision": "928d7a1b1be1870ccfa2",
+    "url": "/NigelMarshal/LoginPage/static/css/1.513c7c3e.chunk.css"
+  },
+  {
+    "revision": "4aa0fbd0204c83ffb8d7b065a12ed3cc",
+    "url": "/NigelMarshal/LoginPage/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/NigelMarshal/LoginPage/precache-manifest.813935d24d8abf122bbb890d1e699577.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/NigelMarshal/LoginPage/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
