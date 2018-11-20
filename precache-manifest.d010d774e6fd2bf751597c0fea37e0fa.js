e15b7aa50ec326e1b621d05e54dc8e43b4dac890
Nigel Marshal <nigelmarshal@gmail.com>
2018-11-20 17:36:08 +0400

    Updates

---
+++ precache-manifest.d010d774e6fd2bf751597c0fea37e0fa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d08f03236335bda05fc570624b88cb58",
+    "url": "/LoginPage/static/media/product-card.d08f0323.png"
+  },
+  {
+    "revision": "895f7fce0d9622283cec51da06cd757a",
+    "url": "/LoginPage/static/media/carrefour-logo.895f7fce.png"
+  },
+  {
+    "revision": "9ff12f694e5951a6f51a9d63b05062e7",
+    "url": "/LoginPage/static/media/OpenSans-Light.9ff12f69.ttf"
+  },
+  {
+    "revision": "d1b16d4f3d11759357ce",
+    "url": "/LoginPage/static/js/runtime~main.d1b16d4f.js"
+  },
+  {
+    "revision": "9f7604f0b8648e04a880",
+    "url": "/LoginPage/static/js/main.9f7604f0.chunk.js"
+  },
+  {
+    "revision": "d97e034292d440251596",
+    "url": "/LoginPage/static/js/1.d97e0342.chunk.js"
+  },
+  {
+    "revision": "9f7604f0b8648e04a880",
+    "url": "/LoginPage/static/css/main.db93066b.chunk.css"
+  },
+  {
+    "revision": "d97e034292d440251596",
+    "url": "/LoginPage/static/css/1.d51a3d3a.chunk.css"
+  },
+  {
+    "revision": "f5d519a39d1ca5e221ea52feeed1fc92",
+    "url": "/LoginPage/index.html"
+  }
+];
